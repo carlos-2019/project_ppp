eace163adc0c477a311bb19915c4176295ca7b76
Jheysson Urbano <jheyssonurb@gmail.com>
2021-04-23 02:14:31 -0500

    Avance Nosotros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule
